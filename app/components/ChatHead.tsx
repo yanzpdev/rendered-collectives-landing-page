e3a139f4ac5d18651e33f4885cbb51ceda163ce2
Arris Ian Peralta <peraltaarris07@gmail.com>
2024-05-24 14:13:34 +0800

    app id replacement for chathead

--- app/components/ChatHead.tsx
+++ app/components/ChatHead.tsx
@@ -3,7 +3,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 
 const ChatHead = () => {
   return (
-    <FacebookProvider appId="1007286433691486" chatSupport>
+    <FacebookProvider appId="709466034466382" chatSupport>
       <CustomChat pageId="149178271620427" minimized={false}/>
     </FacebookProvider> 
   )
